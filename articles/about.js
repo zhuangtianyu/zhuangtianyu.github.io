aa4ef10a69b4c62f5c2c1ef39fad52ee62dec735
zhuangtianyu <zhuangtianyu1995@163.com>
2024-06-19 01:20:11 +0800

    feat: rewrite blog

---
+++ articles/about.js
@@ -0,0 +1,17 @@
+export const body = `
+  # 关于
+
+  ## 我
+
+  研发，北京。
+
+  ## 工具
+
+  TypeScript, React, Node.
+
+  ## 链接
+
+  Email: <a href="mailto:zhuangtianyu1995@163.com">zhuangtianyu1995@163.com</a>
+
+  Github: <a href="https://github.com/zhuangtianyu">https://github.com/zhuangtianyu</a>
+`;
